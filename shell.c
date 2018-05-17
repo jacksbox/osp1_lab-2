ece3dceb4aecbdb1c2e221e6dbc1c1340b0e8b6f
jacksbox <mario.j@jacksbox.de>
2018-05-17 22:04:56 +0200

    first working steps (cd, prog, exit)

--- shell.c
+++ shell.c
@@ -5,8 +5,139 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
+
+#define MAX_CMD_LEN 1024
+#define MAX_CWD_LEN 1024
+
+// shamelessly copied
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
+
+char *getCurrentPath(void) {
+  char cwd[MAX_CWD_LEN];
+  return getcwd(cwd, sizeof(cwd));
+}
+
+int parseCommand(char *tcmd) {
+  // trim whitespaces
+  char *cmd;
+  cmd = trimwhitespace(tcmd);
+
+  // TODO check and implement PIPE
+
+  const char delim[2] = " ";
+  char *token;
+  token = strtok(cmd, delim);
+
+  printf("parse Command: %s\r\n", token); // DEBUG
+
+  int result = 0;
+  if (strcmp(token, "exit") == 0) {
+    // exit console
+    printf("terminating shell...\r\n");
+    result = 1;
+  } else if (strcmp(token, "wait") == 0) {
+    // exit console
+    printf("wait\r\n");
+    result = 1;
+  } else if (strcmp(token, "cd") == 0) {
+    // cd command
+    
+    // get the path (token no. 2)
+    token = strtok(NULL, delim);
+
+    printf("-shell: cd command to %s\r\n", token); // DEBUG
+    
+    if (chdir(token) != 0) {
+      printf("  cd %s: no such Directory\r\n", token);
+    } 
+  } else {
+    // execute program
+    printf("exec\r\n"); // DEBUG
+
+    // create the system exec command
+    char exec_cmd[MAX_CMD_LEN] = "./";
+    strcat(exec_cmd, token);
+
+    // TODO add the command parameters
+
+    printf("exec %s\r\n", exec_cmd); // DEBUG
+          
+    int status = system(exec_cmd);
+    printf("status %d\r\n", status);
+
+    result = 0;
+  }
+
+  return result;
+}
+
+int changeDirectory(char *path) {
+  return chdir(path);
+}
 
 int main(void)
 {
+  char input[MAX_CMD_LEN];
+  char *path;
+
+  int result = 0;
+  while(result == 0) {
+
+    path = getCurrentPath();
+  
+    printf("%s > ", path);
+
+    if (fgets(input, MAX_CMD_LEN, stdin) != NULL) {
+
+      // remove trailing newline
+      char *pos;
+      if ((pos=strchr(input, '\n')) != NULL) {
+        *pos = '\0';
+      }
+
+      if (strcmp(input, "\0") != 0) {
+        // TODO tokenize on & (see below)
+        // fork processes
+        result = parseCommand(input);
+      } else {
+        result = -1;
+      }
+  
+      // tokenize commands
+      // const char delim[2] = "&";
+      // char *token;
+  
+      // token = strtok(input, delim);
+  
+      // while (token != NULL) { 
+      //   printf("%s", token);
+      //   printf("\n\r");
+      //   token = strtok(NULL, delim);
+      // }
+  
+    }
+  }
+
+  printf("error");
+
 	return 0;
 }
